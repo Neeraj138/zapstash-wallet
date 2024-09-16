e6e2bd1ce73184ef8c5b81715e76d47868b9438b
Neeraj138 <neeraj.r@greyorange.com>
2024-09-16 19:24:08 +0530

    minor accessibility changs

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import MainContainer from "./containers/MainContainer";
 
 const App = () => {
   return (
-    <div className="bg-yellow-100 min-h-screen dark:bg-slate-900 dark:text-yellow-50">
+    <div className="bg-yellow-50 min-h-screen dark:bg-slate-950 dark:text-yellow-50">
       <ThemeToggle />
       <div className="text-5xl font-medium text-center p-10 pb-5">
         ZapStash Wallet
